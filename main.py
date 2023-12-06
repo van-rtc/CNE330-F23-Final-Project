8762363d1bfce2784c0f30a491f5097e571909cf
luongvv <127257646+luongvv@users.noreply.github.com>
2023-12-06 10:34:36 -0800

    Mian updated

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 # CNE330 Fall 2023
 # Student name: Van Luong Vuong
 
-import os
-import getpass
-import pwinput
 from cryptography.fernet import Fernet
-
-
 
 # Check if the password is strong
 def check_strong_password(password):
@@ -30,7 +25,7 @@ def check_strong_password(password):
         print("Password is strong!")
         return True
     else:
-        print("Password does not meet the criteria for a strong password for the following reasons:.")
+        print("Password does not meet the criteria for a strong password for the following reasons:")
         if lower_passed == False:
             print('Password does NOT contain at least 1 lowercase letter')
         if upper_count<3:
@@ -47,6 +42,26 @@ def encrypt_password(password, key):
     encrypted_password = cipher.encrypt(password.encode())
     return encrypted_password
 
+#Main
 if __name__ == "__main__":
-    password = input(print("Please enter a password:"))
-    check_strong_password(password)
+    #Step 1: Input password
+    print("Please enter a password:")
+    user_password = input()
+
+    # Step 2: Check if the password is strong
+    if check_strong_password(user_password):
+
+    # Step 3: Encrypt password using Fernet and save it to a file
+        key = Fernet.generate_key()
+        encrypted_password = encrypt_password(user_password, key)
+        with open("encrypted_password.key", "wb") as file:
+            file.write(key)
+            file.write(encrypted_password)
+            print("Password encrypted and saved to 'encrypted_password.key' file.")
+    else:
+        print("Password is not strong. Please ensure it meets the criteria:")
+        print("•	No less than 3 uppercase letters from A to Z.")
+        print("•	At least 1 uppercase letter.")
+        print("•	At least 1 number (0-9).")
+        print("•	At least 1 symbol (!, #, %, @, $, ^, &, *, (, ), -, +, =).")
+
